4d37548657f0a01eb13ecd70d81dd96f4b99321e
Moi-Tech67 <25-15854@g.batstate-u.edu.ph>
2026-05-08 13:59:09 +0800

    add missing flash import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, session, redirect, url_for, flash, send_file
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for, flash
 from datetime import datetime
 import sqlite3
 import qrcode
